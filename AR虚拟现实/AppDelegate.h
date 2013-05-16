d4acb5e574ce1979a3dff8b55b6648b2b3ac5a15
zhangjiajia <ADMIN@admintekiMacBook-Pro.local>
2013-05-16 16:58:46 +0800

    Initial Commit

---
+++ AR虚拟现实/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AR虚拟现实
+//
+//  Created by zhangjiajia on 13-5-16.
+//  Copyright (c) 2013年 bravesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AR虚拟现实/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AR虚拟现实
+//
+//  Created by zhangjiajia on 13-5-16.
+//  Copyright (c) 2013年 bravesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
